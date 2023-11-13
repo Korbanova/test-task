17f326fbc96d87fc631237216cf3829541a362c9
Ekaterina Savoskina <korbanova20@gmail.com>
2023-11-13 18:46:29 +0300

    git init

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CoreModule } from 'core/core.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LayoutComponent } from './shared/layout/layout.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        LayoutComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        CoreModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
